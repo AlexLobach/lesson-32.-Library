6c698018fbf28478eaab008bfca02b660a20f523
AlexLobach <alexlobatch@yandex.ru>
2022-10-24 21:51:57 +0300

    non edit settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -127,5 +127,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-print('hello')
